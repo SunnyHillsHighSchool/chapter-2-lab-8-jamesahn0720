fe748de291057de7f4d3e3b97a08b859fcdf6fba
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-22 23:38:28 -0700

    Update ListFunHouseTwo.java

--- ListFunHouseTwo.java
+++ ListFunHouseTwo.java
@@ -8,113 +8,104 @@ import static java.lang.System.*;
 public class ListFunHouseTwo {
 	private ListNode theList = null;
 
-	// this method will print the entire list on the screen
-	public String toString() {
-		String output = "";
+	 public ListFunHouseTwo() {
+    theList = new ListNode();
+  }
 
-		
-		ListNode first = theList;
-		while(first.getNext() != theList){
-			output+=(String) first.getValue() + ", ";
-			first = first.getNext();
-		}
-		
-		output+=first.getValue();
-		return output;
-	}
+  public void add(Comparable data) {
+    if (theList.getValue() == null) {
+      theList = new ListNode(data, null);
+    } else {
+      theList = new ListNode(data, theList);
+    }
+  }
 
-	// this method will return the number of nodes present in list
-	public int nodeCount() {
-		int count = 1;
-		ListNode mine = theList;
-		while(mine.getNext() != theList){
-			count++;
-			mine = mine.getNext();
-		}
-		return count;
-	}
+  // this method will return the number of nodes present in list
+  public int nodeCount() {
+    ListNode list = theList;
+    int count = 0;
+    while (list != null) {
+      count++;
+      list = list.getNext();
+    }
+    return count;
+  }
 
-	// this method will create a new node with the same value as the first node
-	// and add this
-	// new node to the list. Once finished, the first node will occur twice.
-	public void doubleFirst() {
-		theList.setNext(new ListNode(theList.getValue(),theList.getNext()));
-	}
+  // this method will create a new node with the same value as the first node and
+  // add this
+  // new node at the front of the list. Once finished, the first node will occur
+  // twice.
+  public void doubleFirst() {
+    ListNode secondNode = new ListNode(theList.getValue(), theList.getNext());
+    theList.setNext(secondNode);
+  }
 
-	// this method will create a new node with the same value as the last node
-	// and add this
-	// new node at the end. Once finished, the last node will occur twice.
-	public void doubleLast() {
-			ListNode first = theList;
-			ListNode last = theList;
-			first = first.getNext();
-			while(first != theList){
-				last = first;
-				first = first.getNext();
-			}
-			last.setNext(new ListNode(last.getValue(),theList));
-	}
-	
-	// method skipEveryOther will remove every other node
-	public void skipEveryOther() {
-		removeXthNode(2);
-	}
+  // this method will create a new node with the same value as the last node and
+  // add this
+  // new node at the end. Once finished, the last node will occur twice.
+  public void doubleLast() {
+    ListNode list = theList;
+    while (list.getNext() != null) {
+      list = list.getNext();
+    }
+    list.setNext(new ListNode(list.getValue(), null));
+  }
 
-	// this method will set the value of every xth node in the list
-	public void setXthNode(int x, Comparable value) {
-		ListNode first = theList;
-		ListNode last = theList;
-		int count=1;
-		ListNode temp = theList;
-		first = first.getNext();
-		while(first != theList){
-			if(count % x == 0)
-				temp.setValue(value);
-			temp = temp.getNext();
-			count++;
-		}
-			
-	}
+  // method skipEveryOther will remove every other node
+  public void skipEveryOther() {
+    ListNode list = theList;
+    theList.setNext(list.getNext().getNext());
+    while (list.getNext().getNext() != null) {
+      list.getNext().setNext(list.getNext().getNext());
+      list = list.getNext();
+    }
+    list.setNext(null);
+  }
 
-	// this method will remove every xth node in the list
-	public void removeXthNode(int x) {
+  // this method will set the value of every xth node in the list
+  public void setXthNode(int x, Comparable value) {
+    ListNode list = theList;
+    int count = 1;
+    while (list.getNext() != null) {
+      if ((count - 1) % x == 0) {
+        list.getNext().setValue(value);
+      }
+      list = list.getNext();
+      count++;
+    }
+  }
 
-		int count = 1;
-		ListNode prev = null;
-		ListNode temp = theList;
-		
-		ListNode first = theList;
-		ListNode last = theList;
-		theList = theList.getNext();
-		while(theList != first){
-			if(count % x == 0)
-			{
-				prev.setNext(temp.getNext());
-			}
-			prev = temp;
-			temp = temp.getNext();
-			count++;
-			
-			last = theList;
-			theList = theList.getNext();
-		}
-	}
+  // this method will remove every xth node in the list
+  public void removeXthNode(int x) {
+    ListNode list = theList;
+    int count = 1;
+    ListNode next = null;
+    for (int i = 0; i < x; i++) {
+      next = list.getNext();
+    }
+    while (next.getNext() != null) {
+      if ((count + 1) % x == 0) {
+        list.setNext(list.getNext().getNext());
+        count++;
+      }
+      list = list.getNext();
+      count++;
+      for (int i = 0; i < x; i++) {
+        next = list.getNext();
+      }
+    }
+    list.setNext(null);
+  }
 
-	public void add(String string) {
-	
-		if(theList == null){
-			theList = new ListNode(string, null);
-			theList.setNext(theList);
-		}
-		else{
-			ListNode first = theList;
-			ListNode last = theList;
-			theList = theList.getNext();
-			while(theList != first){
-				last = theList;
-				theList = theList.getNext();
-			}
-			last.setNext(new ListNode(string, first));
-		}
-	}
+  // this method will return a String containing the entire list
+  public String toString() {
+    ListNode list = theList;
+    String output = "";
+    while (list != null) {
+      output += list.getValue() + " ";
+      list = list.getNext();
+    }
+    return output;
+  }
+
 }
